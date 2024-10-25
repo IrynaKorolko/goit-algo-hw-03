97d5ca6af61aa13f29ea000d2e227d2d6d434064
IrynaKorolko <irinapiatenko@gmail.com>
2024-10-25 10:42:35 +0300

    upd 1

--- hometask1.py
+++ hometask1.py
@@ -1,8 +1,12 @@
 from datetime import datetime, date
 def string_to_date(date_string):
-    return datetime.strptime(date_string, "%Y.%m.%d").date()
+    try:
+        return datetime.strptime(date_string, "%Y.%m.%d").date()
+    except ValueError:
+        raise ValueError("Використовуйте формат YYYY.MM.DD")
 
 def get_days_from_today(target_date):
+    target_date = string_to_date(target_date)
     today = date.today()
     delta_days = (target_date - today).days
     return delta_days
