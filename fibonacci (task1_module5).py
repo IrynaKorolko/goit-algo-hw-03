5b97d851a3e5d5c9b00f508b5d7909d1e647f18b
IrynaKorolko <irinapiatenko@gmail.com>
2024-11-14 16:59:41 +0200

    new

---
+++ fibonacci (task1_module5).py
@@ -0,0 +1,16 @@
+def caching_fibonacci():
+    cache = {}
+    def fibonacci(n):
+        if n <=0:
+            return 0
+        if n ==1:
+            return 1
+        if n in cache:
+            return cache[n]
+        if n not in cache:
+            n_fib = fibonacci(n - 1) + fibonacci(n - 2)
+            cache[n] = n_fib
+        return cache[n]
+    return fibonacci
+f = caching_fibonacci()
+print(f(5))
